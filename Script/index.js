4ab9ec68d3528052b41f3f0139a99394c3ac6557
Subroto112001 <subrotokumarbarman@gmail.com>
2025-11-07 10:17:32 +0600

    goto to top button added

--- Script/index.js
+++ Script/index.js
@@ -1,6 +1,26 @@
+// mobile screen menubar fuction
 const menubar = document.getElementById("menubar");
 const nav = document.getElementById("navitems");
 menubar.addEventListener("click", () => {
   nav.classList.toggle("hidden");
   nav.classList.toggle("block");
+});
+
+// goto to top button
+const goToTopBtn = document.getElementById("goToTop");
+window.addEventListener("scroll", function () {
+  if (window.pageYOffset > 200) {
+    goToTopBtn.classList.remove("opacity-0", "invisible");
+    goToTopBtn.classList.add("opacity-100", "visible");
+  } else {
+    goToTopBtn.classList.remove("opacity-100", "visible");
+    goToTopBtn.classList.add("opacity-0", "invisible");
+  }
+});
+
+goToTopBtn.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
